895e523cbda4a4080a2d0ec9e7559e5f814831bb
githubscientist <sathish.datascientist@gmail.com>
2024-04-12 19:18:51 +0530

    template literals

--- index.js
+++ index.js
@@ -1,80 +1,16 @@
-// functions: block of code that gets executed when called
-// function types: library functions and user defined functions
+// let word = "apple";
+// let sentence = 'apple is a fruit';
+// let paragraph = `apple is a fruit and 
+// an apple everyday keeps doctor away!`;
 
-// function definition withuot any arguments and without return type
-// function sayHello() {
-//     console.log("Hello");
-// }
+// console.log(word, typeof word);
+// console.log(sentence, typeof sentence);
+// console.log(paragraph, typeof paragraph);
 
-// sayHello(); // function call
-// sayHello();
+function add(x, y) {
+    // sum of 5 and 6 is 11
+    // console.log('sum of', x, 'and', y, 'is', x+y);
+    console.log(`sum of ${x} and ${y} is ${x+y}`);
+}
 
-// passing value as argument
-// function sayHello(name) {
-//     console.log("Hello", name);
-// }
-
-// // sayHello("Krish");
-// sayHello("Sathish");
-
-// the function with multiple arguments
-// function add(x, y) {
-//     return x + y;
-// }
-
-// console.log(add(5, 6));
-
-/*
-    1. named functions
-    2. anonymous functions
-    3. arrow functions
-    4. IIFE (Immediately Invoked Function Expression)
-*/
-
-// named functions
-// function add(x, y) {
-//     return x + y;
-// }
-
-// console.log(add(5, 6));
-
-// anonymous functions
-// let add = function (x, y) {
-//     return x + y;
-// }
-
-// console.log(add(5, 6));
-
-// arrow functions
-// let add = (x, y) => {
-//     return x + y;
-// }
-
-// console.log(add(5, 6));
-
-// let add = (x, y) => x + y;
-
-// console.log(add(5, 6));
-
-// IIFE: Immediately Invoked Function Expression
-// console.log(((x, y) => {
-//     return x + y;
-// })(5, 6));
-
-// ((x, y) => {
-//     console.log(x + y);
-// })(5, 6)
-
-// console.log((function (x, y) {
-//     return x + y;
-// })(5, 6));
-
-// (function (x, y) {
-//     console.log(x + y);
-// })(5, 6)
-
-let sum = (function (x, y) {
-    return x + y;
-})(5, 6);
-
-console.log(sum);
+add(5, 6);
--- readme.md
+++ readme.md
@@ -15,17 +15,29 @@ ES6:
 - This version is very popular because of introducing new features like arrow functions, classes, template strings, let, const, and many more.
 
 [x] Scoping - var vs let vs const  
-[x] arrow functions  
-[] use of this keyword(lexical scoping)  
-[] template literals  
-[] spread & rest prarameter  
-[] array & object destructure  
-[] property shorthand  
-[] module import & export  
+[x] arrow functions
+
+- Arrow functions are a new way of writing function expressions in JavaScript.
+- They are more concise than traditional function expressions allowing us to write one-liners without the need for the return keyword.
+
+[x] template literals
+
+- Template literals are string literals allowing embedded expressions.
+- You can use multi-line strings and string interpolation features with them.
+
+[] spread & rest prarameter
+[] array & object destructure
+[] property shorthand
 [] Class in Javascript
+[] use of this keyword(lexical scoping)
+[] module import & export
 
 **Pre-read:**
 
 [ES6 Documentation](https://github.com/lukehoban/es6features#readme)
 
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+
+```
+
+```
