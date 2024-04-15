1cc80740b3f601b795942d8ea6657e798f977a79
githubscientist <sathish.datascientist@gmail.com>
2024-04-15 19:37:52 +0530

    module exports and imports

---
+++ main.js
@@ -0,0 +1,9 @@
+// const math = require('./index.js');
+
+// console.log(math.PI);
+// console.log(math.add(2, 3));
+
+const { PI, add } = require('./index.js');
+
+console.log(PI);
+console.log(add(2, 3));
